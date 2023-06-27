cca45d570b14363dfa5db1ca7cd14938b05784ed
clambin <christophe.lambin@gmail.com>
2023-06-27 18:19:27 +0200

    fix: fix readme

--- doc.go
+++ doc.go
@@ -1,12 +1,6 @@
 /*
 Package tado provides an API Client for the Tadoº smart thermostat devices
 
-Using this package typically involves creating an APIClient as follows:
-
-	client := tado.New("your-tado-username", "your-tado-password", "your-tado-secret")
-
-Once a client has been created, you can query tado.com for information about your different Tado devices.
-
 NOTE: the tado package currently only supports heating devices.  Hot water & AC devices are not supported.
 If you have access to these devices, let me know, so I can add support for these in a later release.
 
